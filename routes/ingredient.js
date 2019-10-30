66efbfafa0839e273fd046a142ed0f37c3ab9416
immime12697@gmail.com <ginsum>
2019-10-30 14:24:46 +0900

    [Cnew] serverSetting

---
+++ routes/ingredient.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/lists', (req, res)=>{
+
+});
+
+router.post('/addItem', (req, res)=>{
+  
+});
+
+
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/singin', (req, res)=>{
+
+});
+
+router.post('/singup', (req, res)=>{
+  
+});
+
+router.get('/singout', (req, res)=>{
+  
+});
+
+module.exports = router;
