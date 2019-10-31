742195fe046231b6b07289e5d6b621e7d3fc5bee
최방실 <qkdtlf89089@gmail.com>
2019-10-31 21:20:02 +0900

    [Cnew] signin/up/out and user Api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-var sequelize = require('./models/index').sequelize;
+const session = require('express-session')
+var sequelize = require('./models/index').sequelize
 
 const userRouter = require('./routes/user')
 const ingRouter = require('./routes/ingredient')
@@ -12,6 +13,11 @@ const app = express()
 const port = 4000
 app.use(express.json())
 app.use(cors())
+app.use(
+  session({
+    secret: 'Foodo'
+  })
+)
 
 app.use('/user', userRouter)
 app.use('/grocery', ingRouter)
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,104 @@
 const express = require('express');
 const router = express.Router();
+const crypto = require('crypto')
+var {User} = require('./../models')
 
-router.post('/singin', (req, res)=>{
+let salt = "secondpreference'sfirstproject"
 
+router.get('/', (req,res)=>{
+    User.findAll({
+        attributes: ['email', 'userName', 'createdAt']
+    })
+    .then(result=>{
+        res.send(result)
+    })
+})
+
+router.get('/:id', (req,res)=>{
+    console.log(req.params.id)
+    User.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['email', 'userName', 'createdAt']
+    })
+    .then(data=>{
+        if(data){
+            let info = {
+                email: data.email,
+                userName: data.userName,
+                createdAt: data.createdAt
+            }
+            res.json(info)
+        } else {
+            res.send("invalid user")
+        }
+    })
+})
+
+router.post('/signin', (req, res)=>{
+    let userSession = req.session
+    User
+        .findOne({
+            where: {
+                email : req.body.email,
+                password : crypto.createHmac('sha512',salt)
+                .update(req.body.password)
+                .digest('hex')
+            }
+        })
+        .then((result)=>{
+            if(result){
+                userSession.userId = result.dataValues.userName;
+            }
+            console.dir(userSession.userId)
+            return userSession.userId
+        })
+        .then(result=>{
+            if(result){
+                res.send(`login secceed, ${userSession.userId}`)
+            } else {
+                res.send('login failed')
+            }
+        })
 });
 
-router.post('/singup', (req, res)=>{
-  
+router.post('/signup', (req, res)=>{
+  User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then((result)=>{
+        if(!result){
+            User.create({
+                email: req.body.email,
+                password: crypto.createHmac('sha512',salt)
+                                .update(req.body.password)
+                                .digest('hex'),
+                userName: req.body.userName
+            })
+            .then(result=>{
+                res.status(201).send(result);
+            })
+            .catch(err=>{
+                res.status(500).send(err);
+            })
+        } else if (result) {
+            res.send("email already exists!")
+        }
+    })
 });
 
-router.get('/singout', (req, res)=>{
-  
+router.post('/signout', (req, res)=>{
+    console.log("logout")
+    console.log(req.session.userId)
+  if(req.session.userId){
+      req.session.destroy(err=>{
+          if(err){console.log(err)}
+          else{res.send("bye")}
+      });
+    }
 });
 
 module.exports = router;
