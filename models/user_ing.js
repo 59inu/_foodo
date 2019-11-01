9d2858f472609496a725bb15c4e53c9458376c85
immime12697@gmail.com <ginsum>
2019-11-01 14:39:40 +0900

    [JS] ingredient fixed

--- models/user_ing.js
+++ models/user_ing.js
@@ -1,7 +1,5 @@
 module.exports = (sequelize, DataTypes)=>{
   return sequelize.define('user_ing',{
-    user_id : DataTypes.INTEGER,
-    ing_id : DataTypes.INTEGER,
     exp : DataTypes.STRING,
     quantity : DataTypes.INTEGER,
     memo : DataTypes.STRING,
