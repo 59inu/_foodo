ec7f485fc29c6cc377fbbbf443d74d4ae48b8593
immime12697@gmail.com <ginsum>
2019-11-04 13:09:10 +0900

    [JS] jwt적용

--- routes/ingredient.js
+++ routes/ingredient.js
@@ -4,54 +4,56 @@ const Sequelize = require('sequelize')
 const { Ing, User_Ing } = require('../models')
 
 const Op = Sequelize.Op
-
+const auth = require('./auth')
 
 //회원별 재고 목록 조회
 router.get('/', (req, res) => {
-  const user_id = req.session.userId
-  User_Ing.findAll({
-    where: { userId: 3 },
-    attributes: ['ingredientId', 'exp', 'quantity', 'memo', 'frozen', 'createdAt', 'unit'],
-    include: [Ing]
-  })
-    .then((data) => {
-      const result = []
-      let rest = function (exp) {
-        var today = new Date()  
-        var dateString = exp
-        var dateArray = dateString.split('-')  
-        var dateObj = new Date(dateArray[0], Number(dateArray[1]) - 1, dateArray[2])
-        var restDay = Math.ceil((dateObj.getTime() - today.getTime()) / 1000 / 60 / 60 / 24) 
-        return restDay
-      }
-
-      const message = function(rest) {
-        console.log(rest)
-        if(rest < 0) {
-          return "No Eat"
-        } else if(rest <= 2){
-          return "hurry Eat"
-        } else {
-          return "fresh!"
-        }
-      }
-
-      for (let i = 0; i < data.length; i++) {
-        const ing = {}
-        var restDay = rest(data[i].exp)
-        ing.name = data[i].ingredient.ing_name
-        ing.put = data[i].createdAt
-        ing.rest = restDay 
-        ing.msg = message(restDay)
-        ing.userMemo = data[i].memo
-        ing.frozen = data[i].frozen
-        ing.quantity = data[i].quantity
-        result.push(ing)
-      }
-      res.status(200).json(result)
-    }).catch(err => {
-      res.status(400)
+  auth(req, res, () => {
+    User_Ing.findAll({
+      where: { userId: req.decoded.userId },
+      attributes: ['id', 'ingredientId', 'exp', 'quantity', 'memo', 'frozen', 'createdAt', 'unit'],
+      include: [Ing]
     })
+      .then((data) => {
+        const result = []
+        let rest = function (exp) {
+          var today = new Date()  
+          var dateString = exp
+          var dateArray = dateString.split('-')  
+          var dateObj = new Date(dateArray[0], Number(dateArray[1]) - 1, dateArray[2])
+          var restDay = Math.ceil((dateObj.getTime() - today.getTime()) / 1000 / 60 / 60 / 24) 
+          return restDay
+        }
+
+        const message = function(rest) {
+          console.log(rest)
+          if(rest < 0) {
+            return "No Eat"
+          } else if(rest <= 2){
+            return "hurry Eat"
+          } else {
+            return "fresh!"
+          }
+        }
+
+        for (let i = 0; i < data.length; i++) {
+          const ing = {}
+          var restDay = rest(data[i].exp)
+          ing.id = data[i].id
+          ing.name = data[i].ingredient.ing_name
+          ing.put = data[i].createdAt
+          ing.rest = restDay 
+          ing.msg = message(restDay)
+          ing.userMemo = data[i].memo
+          ing.frozen = data[i].frozen
+          ing.quantity = data[i].quantity
+          result.push(ing)
+        }
+        res.status(200).json(result)
+      }).catch(err => {
+        res.status(400)
+      })
+  })
 })
 
 //전체 재고 목록 조회
@@ -85,89 +87,93 @@ router.get('/:keyword', (req, res) => {
 
 //회원별 재고 추가
 router.post('/addItem', (req, res) => {
-  Ing.findOne({
-    where: {
-      ing_name: req.body.ing_name
-    }
-  })
-    .then(result => {
-      const user_id = req.session.userId
-
-      User_Ing.create({
-        userId: user_id,
-        ingredientId: result.id,
-        exp: req.body.exp, // 날짜
-        quantity: req.body.quantity,
-        memo: req.body.quantity,
-        frozen: req.body.frozen,
-        unit: req.body.unit
+  auth(req, res, () => {
+    Ing.findOne({
+      where: {
+        ing_name: req.body.ing_name
+      }
+    })
+      .then(result => {
+        User_Ing.create({
+          userId: req.decoded.userId
+          ingredientId: result.id,
+          exp: req.body.exp, // 날짜
+          quantity: req.body.quantity,
+          memo: req.body.quantity,
+          frozen: req.body.frozen,
+          unit: req.body.unit
+        })
       })
-    })
-    .then((result) => {
-      res.status(201).json(result)
-    })
-    .catch(err => {
-      res.sendStatus(500)
+      .then((result) => {
+        res.status(201).json(result)
+      })
+      .catch(err => {
+        res.sendStatus(500)
+      })
     })
 })
 
 //회원별 재고량 수정
 router.post('/quantity', (req, res) => {
-  Ing.findOne({
-    where: {
-      ing_name: req.body.ing_name
-    }
-  }).then(result => {
-    const user_id = req.session.userId
-    return User_Ing.findOne({
+  auth(req, res, () => {
+    Ing.findOne({
       where: {
-        userId: user_id,
-        ingredientId: result.id
+        ing_name: req.body.ing_name
       }
+    }).then(result => {
+      const user_id = req.decoded.userId
+      return User_Ing.findOne({
+        where: {
+          userId: user_id,
+          ingredientId: result.id
+        }
+      })
     })
-  })
-    .then(project => {
-      if (project) {
-        project.update({
-          quantity: req.body.quantity
-        })
-      }
-    })
-    .then((result) => {
-      res.status(201).json(result)
-    })
-    .catch(err => {
-      res.sendStatus(500)
+      .then(project => {
+        if (project) {
+          project.update({
+            quantity: req.body.quantity
+          })
+        }
+      })
+      .then((result) => {
+        res.status(201).json(result)
+      })
+      .catch(err => {
+        res.sendStatus(500)
+      })
     })
 })
 
 //회원별 재고량 삭제
 router.post('/delete', (req, res) => {
-  Ing.findOne({
-    where: {
-      ing_name: req.body.ing_name
-    }
-  }).then(result => {
-    const user_id = req.session.userId
-    return User_Ing.findOne({
+  auth(req, res, () => {
+    Ing.findOne({
       where: {
-        userId: user_id,
-        ingredientId: result.id
+        ing_name: req.body.ing_name
       }
-    })
-  })
-    .then(project => {
-      User_Ing.destroy({
+    }).then(result => {
+      const user_id = req.decoded.userId
+      return User_Ing.findOne({
         where: {
-          id: project.id
+          userId: user_id,
+          ingredientId: result.id
         }
       })
     })
-    .then((result) => {
-      res.status(201).json(result)
-    })
-    .catch(err => {
-      res.sendStatus(500)
+      .then(project => {
+        User_Ing.destroy({
+          where: {
+            id: project.id
+          }
+        })
+      })
+      .then((result) => {
+        res.status(201).json(result)
+      })
+      .catch(err => {
+        res.sendStatus(500)
+      })
     })
 })
 
