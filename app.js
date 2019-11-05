1ef9367829c71ef2bdfe41651a9154420c6c016a
최방실 <qkdtlf89089@gmail.com>
2019-11-05 19:40:05 +0900

    add folder controller

--- app.js
+++ app.js
@@ -6,16 +6,17 @@ var sequelize = require('./models/index').sequelize
 const userRouter = require('./routes/user')
 const ingRouter = require('./routes/ingredient')
 const menuRouter = require('./routes/menu')
-const branchRouter = require('./routes/branch')
 const cookieParser = require('cookie-parser')
 
 sequelize.sync()
 
 const app = express()
 const port = 4000
-app.use(cookieParser());
+app.use(cookieParser())
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  credentials: true
+}))
 app.use(
   session({
     secret: 'Foodo'
@@ -26,8 +27,6 @@ app.use('/users', userRouter)
 app.use('/ingredients', ingRouter)
 app.use('/menus', menuRouter)
 
-// 공유주방 라우트
-app.use('/branchs', branchRouter)
 
 app.set('port', port)
 app.listen(app.get('port'))
--- models/branchs.js
+++
@@ -1,5 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('branchs', {
-    branch_name: DataTypes.STRING
-  })
-}
